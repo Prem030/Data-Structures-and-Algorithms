afb0066e0e60276807416e55513bd15d72aaa7d8
Prem030 <premdh8@gmail.com>
2025-11-21 20:08:33 +0530

    array

---
+++ 3.Arrays/Medium/1. Two Sum/optimal.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = 0;
+        vector<int> index;
+        map<int, int> hashMap;
+        
+        for( int i = 0 ; i < n ;i++){
+            
+            int diff = target - nums[ i ];
+            if( hashMap.find( diff ) != hashMap.end()){
+                index.push_back( hashMap[ diff ] );
+                index.push_back( i );
+                break;
+            }
+            hashMap[ nums[ i ]] = i;
+        }
+        return index;
+        
+    }
+int main(){
+    int n;
+    cin>>n;
+    int target;
+    cin>>target;
+    vector<int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        int element;
+        cin>>element;
+        nums.push_back( element );
+    }
+    vector result = twoSum( nums , target );
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout<<result[i]<<" ";
+    }
+    
+    return 0;
+}
