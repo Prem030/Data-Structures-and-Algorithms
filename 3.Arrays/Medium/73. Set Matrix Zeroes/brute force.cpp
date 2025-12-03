13e565c66f1b3a7d023845c8bc7d26a325333e16
Prem030 <premdh8@gmail.com>
2025-12-03 19:09:41 +0530

    array

--- 3.Arrays/Medium/73. Set Matrix Zeroes/brute force.cpp
+++ 3.Arrays/Medium/73. Set Matrix Zeroes/brute force.cpp
@@ -1,47 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void makeZeros( int row , int column , vector<vector<int>> & matrix){
-        for( int i = 0; i < matrix.size() ; i++ )
-            matrix[ i ][ column ] = 0;
-        for ( int j = 0 ; j < matrix[0].size() ; j++){
-            matrix[ row ][ j ] = 0;
-        }
-    }
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> temp;
+ void setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> indexList;
         
-        for( int i = 0 ; i < matrix.size() ; i++){
-            vector<int> temp_row;
-            for( int j = 0 ; j < matrix[ 0 ].size() ; j++ ){
-                temp_row.push_back( matrix[ i ][ j ]);
-            }
-            temp.push_back( temp_row);
-        }
-        for( int i = 0 ; i < matrix.size() ; i++){
-            for( int j = 0 ; j < matrix[ 0 ].size() ; j++ ){
-                cout<<temp[ i ][ j ]<<" ";
-            }
-            cout<<endl;
-        }
-        
+        bool flag = false;
         int row = matrix.size();
-        int column = matrix[ 0 ].size();
-        for( int i = 0 ; i< row ; i++){
+        int column = matrix[0].size();
+        for( int i = 0 ; i <row ; i++){
+            
+            
             for( int j = 0 ; j < column ; j++){
-                if( matrix[ i ][ j ] == 0 ){
-                    makeZeros( i , j , temp);
-                }      
-            }  
-        }
-        for( int i = 0 ; i < matrix.size() ; i++){
-            for( int j = 0 ; j < matrix[ 0 ].size() ; j++ ){
-                matrix[ i ][ j ] = temp[ i ][ j ];
+                vector<int> temp_row;
+                if( matrix[ i ][ j ] == 0){
+                    temp_row.push_back( i );
+                    temp_row.push_back( j );
+                    indexList.push_back( temp_row);
+                    
+                }   
             }
         }
-        
+        for( int i = 0 ; i< indexList.size() ; i++ ){
+            for( int j = 0 ; j < matrix[ 0 ].size() ; j++ ){
+                matrix[ indexList[i][0] ][ j ] = 0;
+            }
+        }
+        for( int i = 0 ; i < indexList.size() ; i++ ){
+            for( int j = 0 ; j < matrix.size() ; j++ ){
+                matrix[ j ][ indexList[ i ][ 1 ]] = 0;
+            }
+        }
     }
-
 int main(){
     int row , column ;
     cin>>row;
